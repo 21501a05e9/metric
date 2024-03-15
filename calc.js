68a085131a8949673a8e7f10dfc789b7ad9ccf91
Rajesh kommu <109227588+21501a05e9@users.noreply.github.com>
2024-03-15 16:21:53 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+function km_to_m(x)
+{
+    return x * 1000;
+}
+function m_to_km(x)
+{
+    return x / 1000;
+}
+function m_to_cm(x)
+{
+    return x * 100;
+}
+function cm_to_m(x)
+{
+    return x / 100;
+}
+exports.km_to_m = km_to_m(x)
+exports.m_to_km = m_to_km(x)
+exports.m_to_cm = m_to_cm(x)
+exports.cm_to_m = cm_to_m(x)
